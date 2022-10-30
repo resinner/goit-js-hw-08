54414d8adbd4f12b107189f4b98600a24a061e7f
resinner <revengingsinner@gmail.com>
2022-10-30 12:40:16 +0200

    Task1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkap = createImageCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkap);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join(``);
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+});
+gallery.on('show.simplelightbox', function (e) {});
