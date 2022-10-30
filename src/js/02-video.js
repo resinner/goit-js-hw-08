ce2d58757d1d3e6db472a3c8f668a4ac88f55f35
resinner <revengingsinner@gmail.com>
2022-10-30 22:40:36 +0200

    Task2 complite

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+const onPlay = throttle((data) => {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+}, 1000);
+
+player.on('timeupdate', onPlay);
+
+player.on("loaded", function () {
+    if (localStorage.getItem(STORAGE_KEY)) {
+        player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+    }
+});
